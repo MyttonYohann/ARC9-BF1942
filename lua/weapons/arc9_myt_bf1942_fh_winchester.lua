b18adb3bd82b60fef97b1401c7de26cf25d0aad6
Mytton Yohann <mynameisnotevenjon+16@gmail.com>
2024-12-17 12:48:02 +0700

    b

--- lua/weapons/arc9_myt_bf1942_fh_winchester.lua
+++ lua/weapons/arc9_myt_bf1942_fh_winchester.lua
@@ -600,13 +600,8 @@ SWEP.Hook_TranslateAnimation = function(wep, curanim)
 	-- reload fuck up --
     local rng = math.Truncate(util.SharedRandom("AV pex last soldier", 1,100))
 	local varextra = 0		-- for att
-	local dementia_end	= 0
 	
 	if wep:HasElement("cal_auto") then varextra = 15
-	end
-	
-	if wep:HasElement("cal_auto") and wep:Clip1() == 6 then dementia_end = 1
-	elseif wep:Clip1() == 8 then	dementia_end = 1
 	end
 
 	if curanim == "reload_insert" or curanim == "reload_emptoloop" then
@@ -622,8 +617,8 @@ SWEP.Hook_TranslateAnimation = function(wep, curanim)
     if rng <= 10 + varextra  then	-- how the dnite be staring at me while my weavile miss all 3 triple axel
 		if	curanim == "reload_insert"	then 	return "reload_loop_fail"	end
 		if	curanim == "cycle"			and wep:Clip1() 	!= 0	then	return "cycle_fail"				end	-- there's nothing in mag to fail, it doesnt cycle on last shot but keeping this just in case i do slam fire
-	elseif rng <= wep.DementiaCounter + varextra then	-- the more you initially have to load the higher the chance of overloading
-		if	curanim == "reload_finish" and dementia_end == 1	then	return "reload_finish_fail"	end	-- overloading an 8th(?) bullet, doesnt work with cal_auto?
+	elseif rng <= wep.DementiaCounter + varextra and wep:Clip1() == wep.ClipSize + wep.ChamberSize then	-- the more you initially have to load the higher the chance of overloading
+		if	curanim == "reload_finish" 	then	return "reload_finish_fail"	end	-- overloading, doesnt work with cal_auto?
 	end
 end
 
